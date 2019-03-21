a0775a36af9bd50ac122537fa4682796da3a4051
DidacRomero <36265669+DidacRomero@users.noreply.github.com>
2019-03-21 22:53:52 +0100

    Small Comment on scene input management

--- FantasyBrawl_Project/Motor2D/j1Scene.cpp
+++ FantasyBrawl_Project/Motor2D/j1Scene.cpp
@@ -135,7 +135,7 @@ bool j1Scene::Update(float dt)
 	if(App->input->GetKey(SDL_SCANCODE_RIGHT) == KEY_REPEAT)
 		App->render->camera.x -= ceil(150.0*dt);
 
-	//Gamepad Test
+	//Gamepad Test. Demonstration on how to use the functions for the gamepads
 	if (IN_RANGE(App->input->GetAxis(PLAYER::P1, SDL_CONTROLLER_AXIS_LEFTY), -40000,-10000))
 		App->render->camera.y += ceil(150.0*dt);
 
