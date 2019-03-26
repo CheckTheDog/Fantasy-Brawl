88366f7632990f6d495079455a14c7301b5cee4e
Vladislav Bulantsev <vladislavbv96@gmail.com>
2019-03-26 19:51:58 +0100

    added more code
    doing all in animation right now, later will be moved to entities.

--- FantasyBrawl_Project/Motor2D/Animation.h
+++ FantasyBrawl_Project/Motor2D/Animation.h
@@ -1,9 +1,10 @@
 #ifndef _ANIMATION_H_
 #define _ANIMATION_H_
 
-#include ""
-
 #include "SDL/include/SDL_rect.h"
+#include "p2List.h"
+#include "j1Module.h"
+
 
 #define MAX_FRAMES 200
 
@@ -23,9 +24,10 @@ private:
 	int last_frame;
 	int loops;
 
+
 public:
 
-	Animation(){}
+	Animation() {}
 
 	Animation(const Animation& anim) :loop(anim.loop), speed(anim.speed), last_frame(anim.last_frame)
 	{
@@ -61,8 +63,51 @@ public:
 		loops = 0;
 	}
 
-	void LoadAnimation(pugi::xml_document &doc, char* node_name)
+	bool Awake(pugi::xml_node&)
 	{
+		
+	}
+
+	void LoadAnimation(const char* file_path, const char* animation_name)
+	{
+		Animation* animation = new Animation();
+
+		bool anim = false;
+
+		pugi::xml_document anim_doc;
+		pugi::xml_parse_result result = anim_doc.load_file(file_path);
+
+
+		if (result == NULL)
+		{
+			LOG("Issue loading animation");
+		}
+
+		pugi::xml_node objgroup;
+		for (objgroup = anim_doc.child("map").child("objectgroup"); objgroup; objgroup = objgroup.next_sibling("objectgroup"))
+		{
+			p2SString name = objgroup.attribute("name").as_string();
+			if (name == animation_name)
+			{
+				anim = true;
+				int x, y, h, w;
+
+				for (pugi::xml_node sprite = objgroup.child("object"); sprite; sprite = sprite.next_sibling("object"))
+				{
+					x = sprite.attribute("x").as_int();
+					y = sprite.attribute("y").as_int();
+					w = sprite.attribute("width").as_int();
+					h = sprite.attribute("height").as_int();
+
+					animation->PushBack({ x, y, w, h });
+				}
+
+			}
+		}
+		if (anim = true)
+			return animation;
+		else
+			return nullptr;
 
 	}
 };
