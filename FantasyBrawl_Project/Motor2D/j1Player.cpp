57ac5dee38340972b9c31194963dde526be42a18
AitorSimona <36265599+AitorSimona@users.noreply.github.com>
2019-04-09 18:42:46 +0200

    Assigning Entity position on creation

--- FantasyBrawl_Project/Motor2D/j1Player.cpp
+++ FantasyBrawl_Project/Motor2D/j1Player.cpp
@@ -71,8 +71,6 @@ bool j1Player::Start()
 	PlayerState = PSTATE::IDLE;
 
 	// --- Current Player Position ---
-	Entityinfo.position.x = 200;
-	Entityinfo.position.y = 0;
 	Future_position.x = Entityinfo.position.x;
 	Future_position.y = Entityinfo.position.y;
 
