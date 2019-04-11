b56109267f518c3fb0fa89fae7516431673fe677
DidacRomero <36265669+DidacRomero@users.noreply.github.com>
2019-04-11 20:24:11 +0200

    Implemented safe area continuous shrinking
    The safe area now shrinks continuously until its gone. it only progresses towards the center.

--- FantasyBrawl_Project/Motor2D/ArenaInteractions.cpp
+++ FantasyBrawl_Project/Motor2D/ArenaInteractions.cpp
@@ -1,4 +1,9 @@
 #include "ArenaInteractions.h"
+#include "j1Render.h"
+#include "j1Timer.h"
+#include "j1PerfTimer.h"
+#include "j1Map.h"
+#include "j1App.h"
 
 ArenaInteractions::ArenaInteractions() : j1Module()
 {
@@ -11,10 +16,46 @@ ArenaInteractions::~ArenaInteractions()
 
 bool ArenaInteractions::Start()
 {
+	StartStorm();
+
+	storm_speed = 5;
+
 	return true;
 }
 
 bool ArenaInteractions::Update(float dt)
 {
+	App->render->DrawQuad(safe_area, 75,0,130,80);
+
+	UpdateStorm();
 	return true;
+}
+
+void ArenaInteractions::StartStorm()
+{
+	//Setet position, width and height of the safe area (AKA eye of the storm)
+	safe_area.x = safe_area.y = 0;
+	safe_area.h = App->map->data.height * App->map->data.tile_height;
+	safe_area.w = App->map->data.width * App->map->data.tile_width;
+
+	//Start the timers
+	storm_timer.Start();
+	storm_update_ptimer.Start();
+}
+
+void ArenaInteractions::UpdateStorm()
+{
+	if(safe_area.w > 0 && safe_area.h > 0)
+	{
+		if (storm_update_ptimer.ReadMs() >= 100)
+		{
+		safe_area.x += storm_speed;
+		safe_area.y += storm_speed;
+
+		safe_area.w -= storm_speed * 2;
+		safe_area.h -= storm_speed * 2;
+
+		storm_update_ptimer.Start();
+		}
+	}
 }
--- FantasyBrawl_Project/Motor2D/ArenaInteractions.h
+++ FantasyBrawl_Project/Motor2D/ArenaInteractions.h
@@ -2,6 +2,12 @@
 #define __ARENAINTERACTIONS_H__
 
 #include "j1Module.h"
+#include "SDL/include/SDL_rect.h"
+#include "j1PerfTimer.h"
+#include "j1Timer.h"
+
+class j1Timer;
+class j1PerfTimer;
 
 class ArenaInteractions : public j1Module
 {
@@ -19,11 +25,21 @@ public: /// Functions
 
 private: /// Functions
 
+	//This function initializes the Storm
+	void StartStorm();
+
+	//For now this will update the size and position of the storm
+	void UpdateStorm();
 
 public: /// Variables
 
+	// Amount of pixels the storm will move each 100 ms
+	int storm_speed;
 
 private: /// Variables
+	SDL_Rect safe_area = {0,0,0,0};
+	j1Timer storm_timer;
+	j1PerfTimer storm_update_ptimer;
 
 };
 
