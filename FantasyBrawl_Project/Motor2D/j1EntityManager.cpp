1731b2d34fe3de29a1e6f8a3eb7762daa3cdb061
AitorSimona <36265599+AitorSimona@users.noreply.github.com>
2019-05-02 17:31:25 +0200

    halved particle and player speeds

--- FantasyBrawl_Project/Motor2D/j1EntityManager.cpp
+++ FantasyBrawl_Project/Motor2D/j1EntityManager.cpp
@@ -95,7 +95,7 @@ bool j1EntityManager::Awake(pugi::xml_node& config)
 	player1info.playerrect = { x,y,w,h };
 
 	// --- P1 Particles ---
-	fPoint particle_speed = { 250.0f, 250.0f };
+	fPoint particle_speed = { 125.0f, 125.0f };
 	player1info.characterdata.basic_attack.anim.PushBack({ 0,0,28,18 });
 	player1info.characterdata.basic_attack.anim.loop = true;
 	player1info.characterdata.basic_attack.life = 2500;
